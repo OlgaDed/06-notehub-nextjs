72656f00b264831f38caaf64fa2ed2e72ace45c2
OlgaDed <garmoneya@ukr.net>
2025-12-07 03:57:52 +0200

    route.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,30 +1,8 @@
-import axios from 'axios';
-import { Note, NoteCreatePayload } from '@/types/note';
-
-const api = axios.create({
-  baseURL: 'https://notes-api.goit.global',
-  headers: {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
-  },
-});
+import type { Note } from '@/types/note';
 
 export async function fetchNotes(): Promise<Note[]> {
-  const response = await api.get('/notes');
-  return response.data;
+  const res = await fetch('/api/notes', {
+    cache: 'no-store',
+  });
+  return res.json();
 }
-
-export async function fetchNoteById(id: string): Promise<Note> {
-  const response = await api.get(`/notes/${id}`);
-  return response.data;
-}
-
-export async function createNote(payload: NoteCreatePayload): Promise<Note> {
-  const response = await api.post('/notes', payload);
-  return response.data;
-}
-
-export async function deleteNote(id: string): Promise<void> {
-  await api.delete(`/notes/${id}`);
-}
-
-console.log('TOKEN:', process.env.NEXT_PUBLIC_NOTEHUB_TOKEN);
